0919427a85e595f49cf82493ef1b7328857fa402
Thiago Moreira Theodoro <thiagomoreiraf273@gmail.com>
2025-05-13 11:23:56 -0300

    P01: criacao do canvas no html

---
+++ P01/main.js
@@ -0,0 +1,15 @@
+function startGame() {
+gameArea.start();
+}
+
+var gameArea = {
+canvas : document.createElement("canvas"),
+start : function() {
+    this.canvas.width = 1400;
+    this.canvas.height = 600;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+}
+}
+
+startGame();
---
+++ README.md
@@ -0,0 +1,5 @@
+# Canvas-Engine
+Projetos menores usando o sistema do canvas do html/javascript
+
+-------
+P01 -> projeto de plataforma baseado em tilesets
