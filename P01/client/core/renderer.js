40350521d1b4fab729c237ba7074fdf2437f87b6
Thiago Moreira Theodoro <thiagomoreiraf273@gmail.com>
2025-05-20 18:00:52 -0300

    P01: teste inicial de atualizar posições

--- P01/client/core/renderer.js
+++ P01/client/core/renderer.js
@@ -2,14 +2,19 @@ import Entity from "./entity.js"
 import f from "../shared/tupple.js";
 import GameScreen from "../game/GameScreen.js";
 import getTiles from "../game/tilelist.js";
+import { getEntities } from "../game/entitylist.js";
 
 const tiles = [];
 const entities = [];
 
 export default function Render(){
     if(!tiles.length) getTiles(tiles);
+    if (!entities.length) entities.push(...getEntities());
 
     tiles.forEach(tile => {
         tile.Drawn();
+    })
+    entities.forEach(entity => {
+        entity.Drawn();
     })
 }
--- P01/client/game/entitylist.js
+++ P01/client/game/entitylist.js
@@ -1,10 +1,16 @@
 import Entity from "../core/entity.js";
 import f from "../shared/tupple.js";
 
-export const entities = [
+export const getEntities = () => [
     {
         name: 'Player',
         id: 1,
-        object: new Entity(f(2, 3), f(0, 0), "#0FE")
+        object: new Entity(f(2, 3), f(0, 0), "#0FE"),
+        Drawn: function() {
+            return this.object.Drawn();
+        },
+        Update: function(dt) {
+            return this.object.Update(dt);
+        }
     }
 ]
